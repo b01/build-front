52d80c38a23bb99af163622a578ff601385512f1
Khalifah K. Shabazz <shabazzk@gmail.com>
2017-07-24 00:06:55 -0400

    Refactored Clean to remove-dirs.

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 const SRC_DIR = __dirname + "/src";
-const Clean = require(SRC_DIR + "/Clean").Clean;
-
-/**
- * @param {array} pPaths
- */
-function clean(pPaths)
-{
-    let clean = new Clean();
-
-    return clean.dirs(pPaths);
-}
 
 exports.lessToCss = require(SRC_DIR + "/lessToCss").lessToCss;
 exports.jsToES5 = require(SRC_DIR + "/transform-js").jsToES5;
-exports.cleanDirs = clean;
+exports.removeDirs = require(SRC_DIR + "/remove-dirs").removeDirs;
